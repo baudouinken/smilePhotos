932fc020f57358a98dafb6165a957d515f6b8c71
Baudouin Keunne <keunne.baudoin@yahoo.fr>
2021-10-19 21:01:37 +0200

    wip: web ui for Gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TeamsComponent } from './components/teams/teams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: GalleryComponent},
+  {path: 'teams', component: TeamsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'contact', component: ContactsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    TeamsComponent,
+    ServicesComponent,
+    ContactsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
