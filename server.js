3b918e85b2e604104b94dd398c35ac7da0aa51a6
Baudouin Keunne <keunne.baudoin@yahoo.fr>
2021-10-19 22:07:00 +0200

    add new config

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//Install express server
+/* //Install express server
 const express = require('express');
 const path = require('path');
 
@@ -8,8 +8,28 @@ const app = express();
 app.use(express.static('./dist/smile-photos'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/smile-photos/index.html'}),
+    res.sendFile('index.html', {root: 'dist/smile-photos/'}),
 );
 
 // Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080); */
+
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist/smile-photos'));
 app.listen(process.env.PORT || 8080);
+
+//PATH LOCATION STARTEGY
+
+app.get('/*', function(req,res){
+  const fullPath = path.join(__dirname + '/dist/smile-photos');
+  console.log(" Fetching from.." + fullPath);
+    res.sendFile(fullPath);
+})
+
+console.log('Server started running..');
+
+//Changed to run on Heroku
