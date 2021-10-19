ec3c5a526dce96a7bcc10949343588a52751b4c9
Baudouin Keunne <keunne.baudoin@yahoo.fr>
2021-10-19 22:23:58 +0200

    update devConfig package json

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-/* //Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -12,24 +12,4 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080); */
-
-
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(__dirname + '/dist/smile-photos'));
 app.listen(process.env.PORT || 8080);
-
-//PATH LOCATION STARTEGY
-
-app.get('/*', function(req,res){
-  const fullPath = path.join(__dirname + '/dist/smile-photos');
-  console.log(" Fetching from.." + fullPath);
-    res.sendFile(fullPath);
-})
-
-console.log('Server started running..');
-
-//Changed to run on Heroku
